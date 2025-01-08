169fe9b38b6259726bdc233e849140b62dd16f66
gopalhingu <gopalhingu123@gmail.com>
2025-01-08 17:53:49 +0530

    resolve live issues

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.post('/html-to-pdf', async (req, res) => {
     try {
         const browser = await puppeteer.launch({
             headless: true,
+            executablePath: '/usr/bin/google-chrome-stable',
             args: ['--no-sandbox', '--disable-setuid-sandbox'],
         });
 
